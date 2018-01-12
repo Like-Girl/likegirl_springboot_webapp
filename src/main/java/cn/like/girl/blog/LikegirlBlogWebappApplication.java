e2251fcaaf1bf3fa6a1514d3c7d5b287e2eb358e
chen.ke <chen.ke@houdezhihui.com>
2018-01-12 11:05:53 +0800

    2018年1月12日11:05:49

---
+++ src/main/java/cn/like/girl/blog/LikegirlBlogWebappApplication.java
@@ -0,0 +1,12 @@
+package cn.like.girl.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikegirlBlogWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikegirlBlogWebappApplication.class, args);
+	}
+}
